14ee1adb860d7ee306f289b53839a4a4ac866772
Miguelangel <miguelangel.dgonzalez@gmail.com>
2022-07-17 16:20:24 -0400

    [FIX] Set on top when change view

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useLocation } from 'react-router-dom';
+
 import '../styles/_vars.scss';
 import '../styles/Layout.scss';
 
@@ -7,8 +9,15 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const layout = ({ children }) => {
+    const {pathname} = useLocation();
+
+    const setOnTop = path => {
+        window.scrollTo(0,0);
+    }
+
     return (
         <div className="layout">
+            {setOnTop(pathname)}
             <Header />
             <div className="content">
                 {children}
